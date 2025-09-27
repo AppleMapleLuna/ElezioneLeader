685303d9f8de5638d53ca9be1f64e1e5f88ef7f9
AppleMapleLuna <huchxi11@gmail.com>
2025-09-27 09:28:35 +0200

    Elezione.py written down

--- Elezione.py
+++ Elezione.py
@@ -1,10 +1,50 @@
-import time
 import threading
 
 TIMEOUT = 5  # secondi di attesa per le risposte OK
 
+# Avvia l'elezione
 def avvia_elezione(nodo):
-    nodo.stato = "leggendo"
+    nodo.stato = "eleggendo"
     print(f"[Nodo {nodo.id}] Avvio elezione...")
-
     
+    # peer = pari, passa nodo per nodo in base a quale è attivo. Ci riesce perché ogni nodo ha una lista di peer.
+    superiori = [peer for peer in nodo.lista_nodi if peer > nodo.id]
+    if not superiori:
+        proclama_leader(nodo)
+        return
+
+    # Invia il messaggio per l'elezione se non ci sono risposte
+    nodo.risposte_ok = False
+    for peer_id in superiori:
+        nodo.invia_messaggio("elezione", peer_id)
+
+    # Avvia un timer per attendere risposte OK
+    timer = threading.Timer(TIMEOUT, verifica_risposte, args=(nodo,))
+    timer.start()
+
+# Controlla se c'è la risposta OK
+def gestisci_risposta_ok(nodo, mittente_id):
+    print(f"[Nodo {nodo.id}] Ricevuto OK da {mittente_id}")
+    nodo.risposte_ok = True
+
+# Verifica le risposte per il coordinatore
+def verifica_risposte(nodo):
+    if not nodo.risposte_ok:
+        proclama_leader(nodo)
+    else:
+        print(f"[Nodo {nodo.id}] Attendo coordinatore...")
+
+# Ora il nodo da normale diventa leader
+def proclama_leader(nodo):
+    nodo.stato = "leader"
+    nodo.leader = nodo.id
+    print(f"[Nodo {nodo.id}] Mi proclamo leader!")
+    for peer_id in nodo.lista_nodi:
+        if peer_id != nodo.id:
+            nodo.invia_messaggio("coordinatore", peer_id)
+
+# Ricevuta coordinatore perché riconosce il leader
+def ricevi_coordinatore(nodo, nuovo_leader_id):
+    nodo.stato = "normale"
+    nodo.leader = nuovo_leader_id
+    print(f"[Nodo {nodo.id}] Riconosco {nuovo_leader_id} come leader.")
